b3f33d3a6bd330e0a73801c600be36da0b812511
Sophia Guo <sophia.gwf@gmail.com>
2023-02-16 11:57:06 -0500

    correct message

--- test/functional/wintps/runInstallerTests.sh
+++ test/functional/wintps/runInstallerTests.sh
@@ -17,7 +17,7 @@ export RESULTS_FOLDER_NAME=$1/report
 export INPUT_FOLDER=$$1/work
 
 mkdir $RESULTS_FOLDER_NAME
-mkdir INPUT_FOLDER
+mkdir $INPUT_FOLDER
 export OTOOL_JDK_VERSION=$2
 export MSI_VENDOR="Adoptium"
 
