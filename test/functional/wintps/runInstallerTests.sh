1d48b021f5557d1d79f7f8857f40668ec6bc8fb9
Sophia Guo <sophia.gwf@gmail.com>
2023-02-27 16:32:14 -0500

    Enable win installer tests
    Signed-off-by: Sophia Guo <sophia.gwf@gmail.com>

--- test/functional/wintps/runInstallerTests.sh
+++ test/functional/wintps/runInstallerTests.sh
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+set -x
+set -eo pipefail
 
 REPORTDIR=$1
 export RESULTS_FOLDER_NAME=$1/report
